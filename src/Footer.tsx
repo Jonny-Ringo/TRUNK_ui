71e8f02935ed9f5b26931797b0ae84986d901d5d
JonnyRingo <92886988+Jonny-Ringo@users.noreply.github.com>
2024-03-17 21:16:21 -0700

    Add files via upload
    update all source files

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -2,37 +2,42 @@ import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <footer className="border-t border-gray text-gray-800 body-font">
-      <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
-        <div className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
-        <Link to={"/"}>
-          <img className='w-48 md:mr-48' src="logo.png" alt="Logo" />
-        </Link>
+    <footer className="bg-gray-100 text-gray-800">
+      <div className="container px-5 py-8 mx-auto grid grid-cols-1 md:grid-cols-3 gap-4 items-center">
+        <div className="md:col-span-1 flex justify-center md:justify-start">
+          <Link to={"/"}>
+            <img className='w-48' src="Trunk Logo.svg" alt="Logo" />
+          </Link>
         </div>
-        <div className="flex-grow sm:pl-8 sm:mt-0 mt-8 sm:text-left text-center">
-          <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">Have Some Fun!</h2>
-          <nav className="list-none mb-10">
-            <li>
-                <Link to={"https://bark.arweave.dev/#/"} className="text-gray-600 hover:text-gray-800">
-                    <div>Swap TRUNK</div>
-                </Link>
-            </li>
-            <li>
-            <Link to={"/vote/"} className="text-gray-600 hover:text-gray-800">
-                    <div>Vote</div>
-                </Link>
-            </li>
-            <li>
-              <a href="https://github.com/Jonny-Ringo/TRUNK_ui" target="_blank" rel="noopener noreferrer" className="text-gray-600 hover:text-gray-800">Contribute</a>
-            </li>
+        <div className="md:col-span-2">
+          <h2 className="title-font font-semibold text-gray-900 text-lg mb-3 text-center md:text-left">Have Some Fun!</h2>
+          <nav className="flex justify-center md:justify-start flex-wrap">
+          
+            <Link to={"https://bark.arweave.dev/#/"} className="mr-5 text-blue-600 hover:text-blue-800 transition duration-300 ease-in-out">
+              <div className="flex items-center">
+                <img src="bark_logo.png" alt="Bark Logo" className="w-4 h-4 mr-2" />
+                Swap TRUNK
+              </div>
+            </Link>
+
+            <Link to={"/vote/"} className="mr-5 text-blue-600 hover:text-blue-800 transition duration-300 ease-in-out">
+              <div className="flex items-center">
+              <img src="vote_icon.png" alt="Vote Icon" className="w-4 h-4 mr-2" />
+                Vote
+              </div>
+            </Link>
+
+            <a href="https://github.com/Jonny-Ringo/TRUNK_ui" target="_blank" rel="noopener noreferrer" className="text-blue-600 hover:text-blue-800 transition duration-300 ease-in-out">
+              <div className="flex items-center">
+              <img src="github_icon.png" alt="Github Logo" className="w-4 h-4 mr-2" />
+                Contribute
+              </div>
+            </a>
           </nav>
-        </div>
-        <div className="flex-grow sm:pl-8 sm:mt-0 mt-8 sm:text-left text-center">
-          
-          
         </div>
       </div>
     </footer>
+
   );
 };
 
--- src/Vote.tsx
+++ src/Vote.tsx
@@ -1,17 +1,17 @@
 import { useEffect, useState } from 'react';
 import { dryrun, message, createDataItemSigner, result } from '@permaweb/aoconnect/browser';
-import { PermissionType } from 'arconnect'
+import { PermissionType } from 'arconnect';
 
 import Footer from './Footer';
 
-const TRUNK = "OT9qTE2467gcozb2g8R6D6N3nQS94ENcaAIJfUzHCww"
+const TRUNK = "OT9qTE2467gcozb2g8R6D6N3nQS94ENcaAIJfUzHCww";
 
 const permissions: PermissionType[] = [
     'ACCESS_ADDRESS',
     'SIGNATURE',
     'SIGN_TRANSACTION',
     'DISPATCH'
-  ]
+];
 
 interface VoteItem {
     tx: string;
@@ -20,25 +20,30 @@ interface VoteItem {
     deadline: number;
 }
 
-const Vote = () => {
-    const [address, setAddress] = useState('')
-    const [voteData, setVoteData] = useState<VoteItem[]>([]);
+interface Tag {
+    name: string;
+    value: string;
+}
 
-    const fetchAddress =  async () => {
-        await window.arweaveWallet.connect(
-            permissions,
-            {
-                name: "TRUNK",
-                logo: "4eTBOaxZSSyGbpKlHyilxNKhXbocuZdiMBYIORjS4f0"
-            }
-        )
+const Vote = () => {
+    const [address, setAddress] = useState('');
+    const [voteData, setVoteData] = useState<VoteItem[]>([]);
+    const [stakeValue, setStakeValue] = useState('');
+    const [unstakeValue, setUnstakeValue] = useState('');
+    const [stakeSuccess, setStakeSuccess] = useState(false);
+
+    const fetchAddress = async () => {
+        await window.arweaveWallet.connect(permissions, {
+            name: "TRUNK",
+            logo: "4eTBOaxZSSyGbpKlHyilxNKhXbocuZdiMBYIORjS4f0"
+        });
         try {
-            const address = await window.arweaveWallet.getActiveAddress()
-            setAddress(address)
-        } catch(error) {
-            console.error(error)
+            const address = await window.arweaveWallet.getActiveAddress();
+            setAddress(address);
+        } catch (error) {
+            console.error(error);
         }
-    }
+    };
 
     const getVotes = async () => {
         try {
@@ -51,7 +56,7 @@ const Vote = () => {
             if (result && result.Messages[0]) {
                 return JSON.parse(result.Messages[0].Data);
             } else {
-                console.log("No readable data from dryrun!")
+                console.log("No readable data from dryrun!");
                 return "";
             }
         } catch (e) {
@@ -61,7 +66,7 @@ const Vote = () => {
     };
 
     const vote = async (id: string, side: string) => {
-        console.log(id, side)
+        console.log(id, side);
         try {
             const getVoteMessage = await message({
                 process: TRUNK,
@@ -78,21 +83,97 @@ const Vote = () => {
                     process: TRUNK,
                 });
                 if (Error) {
-                    alert("Error handling staking:" + Error);
+                    alert("Error handling vote:" + Error);
                     return;
                 }
                 if (!Messages || Messages.length === 0) {
                     alert("No messages were returned from ao. Please try later.");
-                    return; 
+                    return;
                 }
-                alert("Vote successful!")
+                alert("Vote successful!");
             } catch (e) {
-                console.log(e)
+                console.log(e);
             }
         } catch (e) {
-            console.log(e)
+            console.log(e);
         }
-    }
+    };
+
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setStakeValue(event.target.value);
+    };
+
+    const stake = async () => {
+        const value = parseInt(stakeValue);
+        const units = value * 1000;
+        const trunkUnits = units.toString();
+        try {
+            const getStakeMessage = await message({
+                process: TRUNK,
+                tags: [
+                    { name: 'Action', value: 'Stake' },
+                    { name: 'Quantity', value: trunkUnits },
+                    { name: 'UnstakeDelay', value: '1000' },
+                ],
+                signer: createDataItemSigner(window.arweaveWallet),
+            });
+            const { Messages, Error } = await result({
+                message: getStakeMessage,
+                process: TRUNK,
+            });
+            if (Error) {
+                alert("Error handling staking:" + Error);
+                return;
+            }
+            if (!Messages || Messages.length === 0) {
+                alert("No messages were returned from ao. Please try later.");
+                return; 
+            }
+            setStakeSuccess(true);
+        } catch (error) {
+            alert('There was an error staking: ' + error);
+        }
+    };
+
+    const handleUnstakeInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setUnstakeValue(event.target.value);
+    };
+
+    const unstake = async () => {
+        const value = parseInt(unstakeValue);
+        const units = value * 1000;
+        const trunkUnits = units.toString();
+        try {
+            const getUnstakeMessage = await message({
+                process: TRUNK,
+                tags: [
+                    { name: 'Action', value: 'Unstake' },
+                    { name: 'Quantity', value: trunkUnits },
+                ],
+                signer: createDataItemSigner(window.arweaveWallet),
+            });
+            const { Messages, Error } = await result({
+                message: getUnstakeMessage,
+                process: TRUNK,
+            });
+            if (Error) {
+                alert("Error handling unstaking:" + Error);
+                return;
+            }
+            if (!Messages || Messages.length === 0) {
+                alert("No messages were returned from ao. Please try later.");
+                return; 
+            }
+            // Optionally, handle success (e.g., updating UI or state)
+            alert('Unstake successful!');
+        } catch (error) {
+            alert('There was an error unstaking: ' + error);
+        }
+    };
+    
+    
+
+    
 
     useEffect(() => {
         const fetchVotes = async () => {
@@ -105,45 +186,88 @@ const Vote = () => {
         fetchVotes();
     }, [address]);
 
-    return (
-        <div>
-            <div className='flex flex-wrap justify-center h-100 my-40'>
+    function getVoteData() {
+        return (
+          <>
             {voteData.map((item, index) => (
                 <div key={index} className='p-4 border border-gray rounded shadow-md max-w-md lg:max-w-xs sm:max-w-sm w-full md:w-1/2 lg:w-1/4 sm:w-full md:mr-2'>
                     <p className='text-2xl mb-4'>Candidate #{index}</p>
                     <a className='font-bold underline' href={`https://arweave.net/${item.tx}`} target="_blank" rel="noopener noreferrer">Memeframe URL</a>
                     <p className='mt-4'>Yay: {item.yay / 1000}, Nay: {item.nay / 1000}</p>
                     <p>Decided at block: {item.deadline}</p>
-                    {address ? 
-                        (
-                            <div className='text-center mt-4'>
-                                <button onClick={() => vote(item.tx, "yay")} className="bg-black hover:bg-gray-600 text-white font-bold py-2 px-4 rounded mr-4">
-                                    Yes 
+                    {address ? (
+                        <div className='text-center mt-4'>
+                            <button onClick={() => vote(item.tx, "yay")} className="bg-black hover:bg-gray-600 text-white font-bold py-2 px-4 rounded mr-4">
+                                Yes 
+                            </button>
+                            <button onClick={() => vote(item.tx, "nay")} className="bg-red-600 hover:bg-red-400 text-white font-bold py-2 px-4 rounded">
+                                No 
+                            </button>
+                            <p className='text-sm my-2'>Your vote is cast with all the TRUNK you currently have staked.</p>
+                        </div>
+                    ) : (
+                        <div>
+                            <p className='text-center my-4'>
+                                <button onClick={fetchAddress} className="bg-black hover:bg-gray-600 text-white font-bold py-2 px-4 rounded">
+                                    Connect 
                                 </button>
-                                <button onClick={() => vote(item.tx, "nay")} className="bg-red-600 hover:bg-red-400 text-white font-bold py-2 px-4 rounded">
-                                    No 
-                                </button>
-                                <p className='text-sm my-2'>Your vote is cast with all the TRUNK you currently have staked.</p>
-                            </div>
-                        ) :
-                        (
-                            <div>
-                                <p className='text-center my-4'>
-                                    <button onClick={fetchAddress} className="bg-black hover:bg-gray-600 text-white font-bold py-2 px-4 rounded">
-                                        Connect 
-                                    </button>
-                                </p>
-                            </div>
-                        )
-                    }
+                            </p>
+                        </div>
+                    )}
                 </div>
             ))}
+          </>
+        );
+    }
+
+    return (
+        <div>
+            <div className='flex flex-wrap justify-center h-100 my-40'>
+                {voteData.length > 0 ? getVoteData() : <p>No votes yet...</p>}
             </div>
-            
+            {stakeSuccess ? 
+                <p>You have staked TRUNK successfully.</p> :
+                <div className="flex flex-col items-center justify-center">
+                    <input
+                        type="text"
+                        name="stake"
+                        placeholder="Enter TRUNK to stake"
+                        value={stakeValue}
+                        onChange={handleInputChange}
+                        className="py-2 px-4 border rounded-md"
+                    />
+                    <button
+                        className="py-2 px-4 bg-black text-white rounded-md mt-2"
+                        onClick={stake}
+                    >
+                        Stake TRUNK
+                    </button>
+                </div>
+                
+            }
+
+            {/* Unstake input and button */}
+            <div className="flex flex-col items-center justify-center mt-4">
+              <input
+                 type="text"
+                 name="unstake"
+                 placeholder="Enter TRUNK to unstake"
+                 value={unstakeValue}
+                 onChange={handleUnstakeInputChange}
+                 className="py-2 px-4 border rounded-md"
+                  />
+             <button
+               className="py-2 px-4 bg-black text-white rounded-md mt-2"
+               onClick={unstake}
+              >
+              Unstake TRUNK
+              </button>
+            </div>
             <Footer />
         </div>
     );
 };
 
 export default Vote;
+
   
