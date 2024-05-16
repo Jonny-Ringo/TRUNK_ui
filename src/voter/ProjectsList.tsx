a6060c40b738c1508d770f0b71d0aa20be38f9d5
FUDBear <jmackb85@gmail.com>
2024-05-16 12:40:35 -0700

    - Working on the project list page

---
+++ src/voter/ProjectsList.tsx
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from 'react';
+
+interface ProjectInfo {
+    Name: string;
+    IconURL: string;
+    SiteURL: string;
+    Stake: number;
+    Owner: string;
+}
+
+interface ProjectsListProps {
+    Projects: ProjectInfo[];
+}
+
+
+function ProjectsList ( { Projects }: ProjectsListProps ) {
+
+    function FormatStake( amount : number ) {
+        const formated = amount / 1000;
+        return formated;
+    }
+
+    return (
+        <div >
+            
+            <ul className="max-w-md divide-y divide-gray-200 dark:divide-gray-700">
+                {Projects.length > 0 && Projects.map((project, index) => (
+                    <li key={index} className="pt-3 pb-3 sm:pt-4 sm:pb-4">
+                        <a href={project.SiteURL} target="_blank" rel="noopener noreferrer">
+                            <div className="flex items-center space-x-4 rtl:space-x-reverse">
+                                <div className="flex-shrink-0">
+                                    <img className="w-8 h-8 rounded-full" src={project.IconURL} alt={project.Name} />
+                                </div>
+                                <div className="flex-1 min-w-0">
+                                    <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
+                                        {project.Name}
+                                    </p>
+                                    <p className="text-sm text-gray-500 truncate dark:text-gray-400">
+                                        {FormatStake(project.Stake)} | {project.Owner}
+                                    </p>
+                                </div>
+                                <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+                                    #{index +1}
+                                </div>
+                            </div>
+                        </a>
+                    </li>
+                ))}
+            </ul>
+            
+        </div>
+      );
+  }
+  
+  export default ProjectsList;
--- src/voter/VoterModal.tsx
+++ src/voter/VoterModal.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { dryrun, message, createDataItemSigner, result  } from "@permaweb/aoconnect";
 import { PermissionType } from 'arconnect';
 import { GetAddressStakedTrunkAmount, GetTrunkBalance, StakeTrunk, UnstakeTrunk, FetchAddress } from '../app_wheel/MiscTools';
+import ProjectsList from './ProjectsList';
 
 const permissions: PermissionType[] = [
     'ACCESS_ADDRESS',
@@ -17,9 +18,9 @@ interface StakerInfo {
 
 interface ProjectInfo {
     Name: string;
-    Icon: string;
+    IconURL: string;
     SiteURL: string;
-    Stake: string;
+    Stake: number;
     Owner: string;
 }
 
@@ -106,7 +107,8 @@ function VoterModal () {
     }, []);
 
     useEffect(() => {
-        GetTopProject();
+        // GetTopProject();
+        console.log("Projects: ", Projects);
     }, [Projects]);
 
     const GetTopProject = () => {
@@ -147,15 +149,7 @@ function VoterModal () {
 
     return (
         <div className="relative p-8 bg-[#1A1B2D] w-full max-w-md m-auto flex-col flex rounded-lg border-8 border-[#12121C] z-10">
-            {Projects.length > 0 && (
-                <>
-                    <h2 id="projectName">{Projects[0].Name}</h2>
-                    <img id="projectIcon" src={Projects[0].Icon} alt="Project Icon" />
-                    <p id="projectSiteURL">{Projects[0].SiteURL}</p>
-                    <p id="projectStake">{Projects[0].Stake}</p>
-                    <p id="projectOwner">{Projects[0].Owner}</p>
-                </>
-            )}
+            <ProjectsList Projects={Projects} />
         </div>
       );
   }
